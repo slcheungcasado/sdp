15a47dbd05615cf56b78e61cf659ce33bab2259e
Apoorva Katta <apoorvakatta@gmail.com>
2016-10-27 15:13:13 +0800

    1) added addcourse endpoint 2) added html content for course endpoint

--- sdp/courses/urls.py
+++ sdp/courses/urls.py
@@ -7,13 +7,15 @@ app_name = 'courses'
 urlpatterns = [
 	#courses/
     url(r'^$', views.index, name='index'),
-    #/courses/71/
+    #cources/addCourse
+    url(r'^addCourse/', views.addCourse, name='addCourse'),
+    #courses/71/
     url(r'^(?P<course_id>[0-9]+)/$', views.courseDescription, name='courseDescription'),
-    #/courses/71/enroll
+    #courses/71/enroll
     url(r'^(?P<course_id>[0-9]+)/enroll/$', views.enroll, name='enroll'),
-    #/courses/71/addModule
+    #courses/71/addModule
     url(r'^(?P<course_id>[0-9]+)/addModule/$', views.addModule, name='addModule'),
-    #/courses/71/modules/12/addComponent
+    #courses/71/modules/12/addComponent
     url(r'^(?P<course_id>[0-9]+)/modules/(?P<module_id>[0-9]+)/addComponent/$', views.addComponent, name='addComponent')
  
 ]
--- sdp/courses/views.py
+++ sdp/courses/views.py
@@ -8,8 +8,15 @@ from staff.models import Instructor, Participant
 
 def index(request):
 	#return all courses
-	courses = Course.objectes.all().filter(isPublished = True)
-	return render(request, '/courses/courses.html', {'courses': courses})
+	courses = Course.objects.all().filter(isPublished = True)
+	return render(request, 'courses/courses.html', {'courses': courses})
+
+def addCourse(request):
+	course = Course(instructor = request.POST['instructor'], courseCode = request.POST['courseCode'], category  = request.POST['category'],
+	isPublished = request.POST['isPublished'], title = request.POST['title'], description = request.POST['description'])
+	course.save()
+
+	return render(request, 'courses/addCourse.html')
 
 def courseDescription(request, course_id):
 	course = get_object_or_404(Course, pk = course_id)
@@ -18,12 +25,11 @@ def courseDescription(request, course_id):
 		instructor = Instructor.objects.get(pk = request.GET['insID'])
 	except Instructor.DoesNotExist:
 		## display appropriate error message with authorization fails
-		return render(request, '/courses/err.html', {'message': "Instructor with the ID does not exits"})
+		return render(request, 'courses/err.html', {'message': "Instructor with the ID does not exits"})
 
 	isCourseInstructor = course.instructor == instructor.id 
 	## show appropriate course description page depending on whether participant or instructor
-	return render(request, '/courses/course_description.html', {'course': course, 'isCourseInstructor': isCourseInstructor})
-
+	return render(request, 'courses/course_description.html', {'course': course, 'isCourseInstructor': isCourseInstructor})
 
 def addModule(request, course_id):
 
@@ -31,7 +37,7 @@ def addModule(request, course_id):
 	try:
 		course = Course.objects.get(pk = course_id)
 	except Course.DoesNotExist:
-		return render(request, '/courses/err.html', {'message': 'Course does not exist'})
+		return render(request, 'courses/err.html', {'message': 'Course does not exist'})
 
 	## POST request
 	## Instructor id provided as post params. Check if course is created by course instructor
@@ -39,26 +45,25 @@ def addModule(request, course_id):
 		instructor = Instructor.objects.get(pk = request.POST['insID'])
 	except Instructor.DoesNotExist:
 		## display appropriate error message with authorization fails
-		return render(request, '/courses/err.html', {'message': "Instructor with the ID does not exits"})
+		return render(request, 'courses/err.html', {'message': "Instructor with the ID does not exits"})
 
 	isCourseInstructor = course.instructor == instructor.id
 
 	if isCourseInstructor == False:
-		return render(request, '/courses/err.html', {'message': 'You are not allowed to add modules to the course'})
+		return render(request, 'courses/err.html', {'message': 'You are not allowed to add modules to the course'})
 
 	components = request.POST['components']
 	module = Module.components_set.create(components)
 	module.sequenceNumber = request.POST['sequenceNumber']
 	module.course = course_id
 
-	return render(request, '/courses/modules.html', {'module': module, 'isInstructor': isInstructor})
-
+	return render(request, 'courses/modules.html', {'module': module, 'isInstructor': isInstructor})
 
 def addComponent(request, course_id, module_id):
 	try:
 		course = Course.objects.get(pk = course_id)
 	except Course.DoesNotExist:
-		return render(request, '/courses/err.html', {'message': 'Course does not exit'})
+		return render(request, 'courses/err.html', {'message': 'Course does not exit'})
 
 
 	## POST request
@@ -67,26 +72,25 @@ def addComponent(request, course_id, module_id):
 		instructor = Instructor.objects.get(pk = request.POST['insID'])
 	except Instructor.DoesNotExist:
 		## display appropriate error message with authorization fails
-		return render(request, '/courses/err.html', {'message': "Instructor with the ID does not exits"})
+		return render(request, 'courses/err.html', {'message': "Instructor with the ID does not exits"})
 
 	isCourseInstructor = course.instructor == instructor.id
 
 	if isCourseInstructor == False:
-		return render(request, '/courses/err.html', {'message': 'You are not allowed to add modules to the course'})
+		return render(request, 'courses/err.html', {'message': 'You are not allowed to add modules to the course'})
 
 
 	## get module. Display err if module not present
 	try:
 		module = Module.objects.get(pk = module_id)
 	except Module.DoesNotExist:
-		return render(request, '/courses/err.html', {'message': 'Module does not '})
+		return render(request, 'courses/err.html', {'message': 'Module does not '})
 
 	component = Component(module=module_id,order =request.POST['order'], 
 		contentType=request.POST['contentType'], content=request.POST['content'])
 	component.save()
 
-	return render(request, '/courses/module_details.html', {'module': module})
-
+	return render(request, 'courses/module_details.html', {'module': module})
 
 def publishCourse(request, course_id):
 	## POST request
@@ -95,20 +99,18 @@ def publishCourse(request, course_id):
 		instructor = Instructor.objects.get(pk = request.POST['insID'])
 	except Instructor.DoesNotExist:
 		## display appropriate error message with authorization fails
-		return render(request, '/courses/err.html', {'message': "Instructor with the ID does not exits"})
+		return render(request, 'courses/err.html', {'message': "Instructor with the ID does not exits"})
 
 
 	## appropriate error message if course does not exist
 	try:
 		course = Course.objects.get(pk = course_id)
 	except Course.DoesNotExist:
-		return render(request, '/courses/err.html', {'message': 'Course does not exit'})
+		return render(request, 'courses/err.html', {'message': 'Course does not exit'})
 
 	course.isPublished = True
 	course.save()
-	return render(request, '/courses/course_details.html', {'course': course})
-
-
+	return render(request, 'courses/course_details.html', {'course': course})
 
 def enroll(request, course_id):
 	##POST request
@@ -117,13 +119,13 @@ def enroll(request, course_id):
 		participant = Participant.objects.get(pk = request.POST['parID'])
 	except Participant.DoesNotExist:
 		# err if participant does not exist
-		return render(request, '/courses/err.html', {'message': 'Participant with the id does not exist'})
+		return render(request, 'courses/err.html', {'message': 'Participant with the id does not exist'})
 
 	# err if course does not exist
 	try:
 		course = Course.objects.get(pk = course_id)
 	except Course.DoesNotExist:
-		return render(request, '/courses/err.html', {'message': 'Course does not exit'})
+		return render(request, 'courses/err.html', {'message': 'Course does not exit'})
 
 	enroll = Enrollment(course=course_id, participant_id=participant.id)
-	return render(request, '/staff/course_description.html', {'course': course})
+	return render(request, 'staff/course_description.html', {'course': course})
